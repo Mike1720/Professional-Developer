0c9690f3818c02e7ec8e52c896a6dbb5617ce306
Miguel Angel Miranda Velasco <mikemv1720@hotmail.com>
2025-04-11 18:28:21 -0600

    01-Variables y tipos de datos

---
+++ 05-Programacion_Imperativa_JS/B-Introduccion-JS/01-Variables-y-tipos-de-datos.js
@@ -0,0 +1,45 @@
+// DECLARACION DE VARIABLE
+/* 
+  Para declarar variables se hace uso de la palabra reservada "let".
+  Es necesario saber que los nombres de variables deben ser palabras descriptivas del valor que almacenarán
+    -No puede COMENZAR con números
+*/
+
+// Tipos de datos
+
+// String o Cadena de caracteres
+let string = "Cadena de caracteres"
+
+// Number o Numerico
+let number = 25
+
+// Float o Flotante
+let float = 25.17
+
+// Boolean o Boleano
+let True = true
+let False = false
+
+// Undefined o Indefinido
+let Indefinido
+
+// Null
+let vacio = null
+
+// Diferencia entre null y undefined
+/* 
+  Null se ocupa para declarar una variable vacia, mientras que undefined es la ausencia de valor.
+  Usando una analogia seria como
+  Variable declarada -> Botella de agua llena
+  Null -> tener una botalla de agua vacia.
+  Undefined -> No tener una botella
+*/
+
+// Mostrar por pantalla los valores de las variables
+console.log(string); // output -> Cadena de caracteres
+console.log(number); // output -> 25
+console.log(float); // output -> 25.17
+console.log(True); // output -> true
+console.log(False); // output -> false
+console.log(Indefinido); // output -> undefined
+console.log(vacio); // output -> null
