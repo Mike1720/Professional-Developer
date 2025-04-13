e96cd70b8af3fc9cfd4f365e6042898e5af62217
Miguel Angel Miranda Velasco <mikemv1720@hotmail.com>
2025-04-12 19:37:44 -0600

    03-Operadores Aritméticos

---
+++ 05-Programacion_Imperativa_JS/B-Introduccion-JS/03-Operadores-aritmeticos.js
@@ -0,0 +1,32 @@
+// Operadores Aritmeticos
+
+// Variables
+let n1 = 10
+let n2 = 25
+let n3 = 109
+let n4 = 90
+let n5 = 2
+
+// Suma
+let resSuma = n1 + n2 + n3 + n4
+console.log(resSuma); // output -> 234
+
+// Resta
+let resResta = n2 - n1
+console.log(resResta); // output -> 15
+
+// Division o Cociente
+let resDivisión = n4 / n1
+console.log(resDivisión); // output -> 9
+
+// Multiplicación
+let resMultiplicación = n3 * n2
+console.log(resMultiplicación); // output -> 2725
+
+// Potencia
+let resPotencia = n1 ** n5
+console.log(resPotencia); // output -> 100
+
+// Modulo o Residuo
+let resModulo = n2 % n1
+console.log(resModulo); // output -> 5
