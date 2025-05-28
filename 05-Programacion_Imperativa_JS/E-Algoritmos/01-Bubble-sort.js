681e6e7d3d160ffecce9e77221ccc9f8e9c9fcd6
Miguel Angel Miranda Velasco <mikemv1720@hotmail.com>
2025-05-27 21:09:23 -0600

    01-Bubble sort

---
+++ 05-Programacion_Imperativa_JS/E-Algoritmos/01-Bubble-sort.js
@@ -0,0 +1,39 @@
+// Bubble sort - ordenamiento burburja
+/* 
+  El algoritmo Bubble sort realiza el recorrido de un array para ordenarlo de menor a mayor o visceversa.
+  Esto lo hace mediante una comparación de elementos, de manera que compara un elemento en la posición "n" con
+  otro en la posición "n + 1"
+  Ejemplo: n = 0; n + 1 = 1
+  En caso de que el valor de la posición "n + 1", sea menor que el valor de la posición "n", entonces se invertiran
+  los valores. Esta acción se repetira cuantas veces sea necesario hasta que el arreglo este ordenado en la manera
+  indicada
+*/
+
+/**
+ * 
+ * @param {Array} array 
+ * @returns new ordered array
+ */
+function bubbleSort(array) {
+  let newArr = [...array]
+  for (let j = 0; j < newArr.length; j++) {
+    let swapped = false
+    for (let i = 0; i < newArr.length - 1 - j; i++) {
+      if (newArr[i] > newArr[i + 1]) {
+        let support = newArr[i + 1]
+        newArr[i + 1] = newArr[i]
+        newArr[i] = support
+        swapped = true
+      }
+    }
+    if (!swapped) break
+  }
+  return newArr
+}
+let orderedArr
+let arr = [3, 1, 3, 22, 2, 97, 101, 1]
+let arr2 = [1, 1, 12, 2, 2, 3, 6, 10]
+orderedArr = bubbleSort(arr)
+console.log(orderedArr);
+orderedArr = bubbleSort(arr2)
+console.log(orderedArr);
