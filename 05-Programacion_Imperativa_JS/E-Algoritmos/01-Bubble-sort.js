dba7c1fc5dc238f26612b1fb026524a882d4b4ea
Miguel Angel Miranda Velasco <mikemv1720@hotmail.com>
2025-06-04 20:52:18 -0600

    Agregado de comentarios

--- 05-Programacion_Imperativa_JS/E-Algoritmos/01-Bubble-sort.js
+++ 05-Programacion_Imperativa_JS/E-Algoritmos/01-Bubble-sort.js
@@ -15,20 +15,34 @@
  * @returns new ordered array
  */
 function bubbleSort(array) {
-  let newArr = [...array]
+  let newArr = [...array] // Copia exacta del arreglo original
+  // Iteración del arreglo "j" veces
   for (let j = 0; j < newArr.length; j++) {
-    let swapped = false
+    let swapped = false // -> Indicador si hubo intercambios en las vueltas
+    // Iteración de los elementos del arreglo
     for (let i = 0; i < newArr.length - 1 - j; i++) {
+      /* 
+        - 1: Se resta 1 unidad para evitar la comparación del ultimo elemento con undefined
+        - j: Se resta los valores de "j" para evitar seguir la comparación del ultimo elemento, sabiendo que los ultimos elementos
+            colocados en cada iteración han sido ordenados.
+            Ej. [7,4,2,1]
+            Ejecutando la función el resultado de cada iteración será:
+            0.- [4,2,1,7] -> Ultimo elemento ordenado desde la primer iteración
+            1.- [2,1,4,7] -> Penultimo elemento ordenado en la seguda iteración. No fue necesario realizar la comparación 4 > 7, ya que el ultimo elemento estaba ordenado
+            2.- [1,2,4,7] -> Elementos restantes ordenados en la siguiente iteración. No fue necesario comparar 2 > 4, ni 4 > 7.
+
+      */
+      //  currentValue > nextValue
       if (newArr[i] > newArr[i + 1]) {
-        let support = newArr[i + 1]
-        newArr[i + 1] = newArr[i]
-        newArr[i] = support
-        swapped = true
+        let support = newArr[i + 1] // Asiganción de variable temporal con el primer valor a que será sobreescrito
+        newArr[i + 1] = newArr[i] // Actualización del valor nextValue por el valor de currentValue
+        newArr[i] = support // Actualización del valor currentValue por el valor de nextValue que fue almacenado en la variable temporal
+        swapped = true // Indicación que en la vuelta "j" hubo cambio
       }
     }
-    if (!swapped) break
+    if (!swapped) break // Si no hay cambios, se indica que el arreglo esta ordenado y no es necesario realizar más iteraciones.
   }
-  return newArr
+  return newArr // Retorno del nuevo arreglo ordenado
 }
 let orderedArr
 let arr = [3, 1, 3, 22, 2, 97, 101, 1]
